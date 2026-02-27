3b0d7957437e461f8c2af36247fb93ba4c4879d6
annalu0606 <annaluizaramoslr@gmail.com>
2026-02-27 12:24:41 -0300

    Add files via upload

---
+++ ex.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ler_dado(char texto[]) {
+
+    int t;
+
+    printf("%s", texto);
+    scanf("%d", &t);
+    return t;
+
+
+
+
+}
+
+int calcula_area(int b, int h) {
+    return b * h;
+}
+
+int main () {
+
+    int base, altura, area;
+
+    base = ler_dado("Digite a base: ");
+    altura = ler_dado("Digite a altura: ");
+
+    area = calcula_area(base, altura);
+    printf("Area = %d\n", area);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
